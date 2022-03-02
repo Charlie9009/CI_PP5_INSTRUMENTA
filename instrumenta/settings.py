44b6837fdc0574e1f7f12857559a28c77f1073ca
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-02 18:56:34 +0000

    SETTINGS.PY: Add path to database on heroku

--- instrumenta/settings.py
+++ instrumenta/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'instrumenta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
