a4d07065cd85093ad72fcc622a12a01a7efb402a
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-12 18:16:01 +0000

    MODELS.PY: Create a new class model product

--- products/models.py
+++ products/models.py
@@ -19,3 +19,22 @@ class Category(models.Model):
         Returns the category friendly name
         """
         return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    The class for the products model
+    """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """
+        Returns the product name
+        """
+        return self.name
