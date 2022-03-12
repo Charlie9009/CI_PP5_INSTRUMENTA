5f554c7a4047b7147624ab65bed9df32491b682c
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-12 18:35:09 +0000

    ADMIN.PY: Add class for seeing categories in admin

--- products/admin.py
+++ products/admin.py
@@ -7,12 +7,17 @@ class ProductAdmin(admin.ModelAdmin):
     Class for the product model
     """
     list_display = ('name', 'category', 'description',
-                    'price', 'rating', 'image', 'image_url')
-    list_filter = ('name', 'category', 'price')
-    search_fields = ('name', 'category', 'price')
+                    'price', 'rating', 'image', 'image_url',)
+    list_filter = ('name', 'category', 'price',)
+    search_fields = ('name', 'category', 'price',)
 
 
-
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Class for the category model
+    """
+    list_display = ('friendly_name', 'name',)
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
