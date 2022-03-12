fcb8a81b14be0a5a2e5b82f63bbfd9b0edcbc426
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-12 18:34:45 +0000

    ADMIN.PY: Add class for seeing products in admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Class for the product model
+    """
+    list_display = ('name', 'category', 'description',
+                    'price', 'rating', 'image', 'image_url')
+    list_filter = ('name', 'category', 'price')
+    search_fields = ('name', 'category', 'price')
+
+
+
+
+
+admin.site.register(Product, ProductAdmin)
