b88d3ae5588bdbc7e8b751ec4725c4fa140f833c
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-12 17:53:08 +0000

    MODELS.PY: Create a new class for categories

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    The class for the category model
+    """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """
+        Returns the category name
+        """
+        return self.name
+
+    def get_friendly_name(self):
+        """
+        Returns the category friendly name
+        """
+        return self.friendly_name
