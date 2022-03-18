3455725c35d26b3ac5f6567e47d272b3b0e1e1e6
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-18 12:06:55 +0000

    VIEWS.PY: Add functionality for toast message when updating product in bag

--- bag/views.py
+++ bag/views.py
@@ -19,6 +19,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
