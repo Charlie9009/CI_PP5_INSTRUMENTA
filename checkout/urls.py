0a04e02739abd68a820ef50e18ca61834d2779f5
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-21 08:56:02 +0000

    URLS.PY: Remove views from webhook url

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>',
          views.checkout_success, name='checkout_success'),
-    path('wh/', views.webhook, name='webhook'),
+    path('wh/', webhook, name='webhook'),
 ]
