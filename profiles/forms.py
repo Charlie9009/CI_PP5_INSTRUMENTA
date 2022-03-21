5a64580a3bfaf853f85674daff201ac1e61e26f2
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-21 19:50:34 +0000

    CHECKOUT: Add email templates

--- profiles/forms.py
+++ profiles/forms.py
@@ -37,4 +37,4 @@ class UserProfileForm(forms.ModelForm):
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
-            self.fields[field].label = False 
+            self.fields[field].label = False
