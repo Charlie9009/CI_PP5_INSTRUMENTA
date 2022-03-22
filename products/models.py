9a89556ed8c2c3d94f2ca60468eb178420043851
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-22 19:08:35 +0000

    MODELS.PY: Create review model and update image field to be required

--- products/models.py
+++ products/models.py
@@ -1,18 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-
+    """
+    The class for the category model
+    """
     class Meta:
         """
         Change spelling on admin page
         """
         verbose_name_plural = 'Categories'
-    """
-    The class for the category model
-    """
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    name = models.CharField(
+        max_length=254
+    )
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         """
@@ -31,16 +38,67 @@ class Product(models.Model):
     """
     The class for the products model
     """
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.CharField(max_length=254)
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    name = models.CharField(
+        max_length=254
+    )
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    image_url = models.URLField(
+        max_length=1024,
+        null=True,
+        blank=True
+    )
+    image = models.ImageField(
+        null=False,
+        blank=False
+    )
 
     def __str__(self):
         """
         Returns the product name
         """
         return self.name
+
+
+class Review(models.Model):
+    """
+    The class for the review model
+    """
+    user = models.ForeignKey(
+        User,
+        models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product,
+        models.CASCADE
+    )
+    comment = models.TextField(
+        max_length=240
+    )
+    rating = models.IntegerField(
+        default=0
+    )
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        """
+        Returns the comment
+        """
+        return self.comment
