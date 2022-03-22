35643808c70f479b041058fe1a121f8d61a1991b
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-22 19:16:00 +0000

    ADMIN.PY: Add admin page for review

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Review
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -19,5 +19,16 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('friendly_name', 'name',)
 
 
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    Class for the product model
+    """
+    list_display = ('user', 'product', 'comment',
+                    'rating', 'created_on',)
+    list_filter = ('user', 'product', 'comment', 'rating',)
+    search_fields = ('user', 'product', 'comment', 'rating',)
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review, ReviewAdmin)
