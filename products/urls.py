e4890cbbc9b12eaeb2bd1d99a38f36ce7c7798f0
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-23 10:36:28 +0000

    URLS.PY: Add path for review url

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('review/<int:product_id>/', views.review, name='review'),
 ]
