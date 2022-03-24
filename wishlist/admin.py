82b1d031918132403fbc3f8607db5fc228395670
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-24 19:46:26 +0000

    ADMIN.PY: Add admin page

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Wishlist
 
-# Register your models here.
+
+class WishlistAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Favourites model.
+    """
+    list_display = ('user',)
+    search_fields = ('user', 'products',)
+    list_filter = ('user', 'products',)
+
+
+admin.site.register(Wishlist, WishlistAdmin)
