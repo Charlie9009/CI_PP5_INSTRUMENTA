6abf07a576da04bac9b4e230c0bb4aaacbdce090
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-24 18:27:08 +0000

    MODELS.PY: Add new model and migrate

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from products.models import Product
+
+
+
+class Wishlist(models.Model):
+    """
+    A model for a users wishlist
+    """
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    products = models.ManyToManyField(
+        Product,
+        blank=True
+    )
+
+    def __str__(self):
+        """
+        Return user wishlist
+        """
+        return f"{self.user}'s wishlist"
