54433c6ae02350cd01064f0a6099a51997cd0fce
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-24 19:45:50 +0000

    VIEWS.PY: Code for rendering a users whishlist and except indexerror when list is empty

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from .models import Wishlist
 
 
 def wishlist(request):
     """
     View to render all wishlist items
     """
+    try:
+        wishlist_objects = Wishlist.objects.filter(user=request.user.id)[0]
+        wishlist_items = wishlist_objects.products.all()
+    except IndexError:
+        wishlist_items = None
     template = 'wishlists/wishlist.html'
+    context = {
+        'wishlist_items': wishlist_items,
+    }
 
-    return render(request, template)
-    
+    return render(request, template, context)
