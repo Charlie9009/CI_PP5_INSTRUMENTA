23c97616344a6c48e4e502b39f4c9dae4739fba9
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-24 11:02:49 +0000

    QUESTIONS: Add view for question, migrate model, add html file, hook up urls

--- instrumenta/settings.py
+++ instrumenta/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'questions',
 
     # Other
     'crispy_forms',
--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Question
+from .forms import QuestionForm
 
-# Create your views here.
+
+@login_required
+def questions(request):
+    """
+    A view to add a question
+    """
+    question = Question.objects.all()
+    question_form = QuestionForm(request.POST)
+    if request.method == 'POST':
+        if question_form.is_valid():
+            Question.objects.create(
+                        user=request.user,
+                        question_categories=request.POST['question_categories'],
+                        question_comment=request.POST['question_comment'],
+                )
+            messages.success(request, 'Thank you for your question!')
+        else:
+            messages.error(
+                            request,
+                            'Something went wrong')
+
+            return redirect(reverse('questions'))
+
+    template = 'questions/questions.html'
+    context = {
+        'question_form': question_form,
+        'question': question,
+    }
+
+    return render(request, template, context)
