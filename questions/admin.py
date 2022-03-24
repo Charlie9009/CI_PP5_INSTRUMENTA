9de4457b64a9a4afe684758af6d0439252635293
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-24 11:03:40 +0000

    ADMIN.PY: Add Admin page

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    Class for the product model
+    """
+    list_display = ('user', 'question_categories', 'question_comment',
+                    'created_on',)
+    list_filter = ('user', 'question_categories', 'question_comment', 'created_on',)
+    search_fields = ('user', 'question_categories', 'question_comment', 'created_on',)
+
+
+admin.site.register(Question, QuestionAdmin)
