ac77002c70e4b95643613a5693392260379e2463
Charlie9009 <charlie.gustafsson90@gmail.com>
2022-03-25 19:12:08 +0000

    PROJECT: Add comments and fix for pep8

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,13 +3,16 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-def bag_contents(request):
 
+def bag_contents(request):
+    """
+    A function that has all the bag contents
+    """
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
-
+    # Calculate the product price
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.price
@@ -19,16 +22,16 @@ def bag_contents(request):
             'quantity': quantity,
             'product': product,
         })
-
+    # Calculate delivery cost towards the grand total
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+    # return the contexts
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -6,21 +6,21 @@ from products.models import Product
 
 def view_bag(request):
     """ A view to show the bag """
-
     return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
-
+    # Update a product already in bag or add new one to bag
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.success(
+                         request,
+                         f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
@@ -31,14 +31,15 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of a specified product to a specified amount"""
-
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
-
+    # # Update the quantity in the bag or remove it
     if quantity > 0:
         bag[item_id] = quantity
-        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+        messages.success(
+                         request,
+                         f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
         messages.success(request, f'Removed {product.name} from your bag')
@@ -49,7 +50,6 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
-
     try:
         product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
@@ -61,4 +61,4 @@ def remove_from_bag(request, item_id):
 
     except Exception as e:
         messages.error(request, f'Error removing product: {e}')
-        return HttpResponse(status=500) 
+        return HttpResponse(status=500)
