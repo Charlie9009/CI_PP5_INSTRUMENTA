daafd08190a20cad35074047910f973dedbfd6f5
Charlie9009 <charlie.gustafsson90@gmail.com>
2025-04-03 07:36:07 +0000

    Fix issues

--- instrumenta/settings.py
+++ instrumenta/settings.py
@@ -191,7 +191,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'instrumenta-north'
+    AWS_STORAGE_BUCKET_NAME = 'instrumenta'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
